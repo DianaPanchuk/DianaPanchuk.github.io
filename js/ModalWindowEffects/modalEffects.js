431a734832f7cec232f4092471b9052fda5110db
Diana <dianochkaua@mail.ru>
2017-05-23 14:53:02 +0300

    upd

--- js/ModalWindowEffects/modalEffects.js
+++ js/ModalWindowEffects/modalEffects.js
@@ -1,4 +1,4 @@
-'use strict';/**
+/**
  * modalEffects.js v1.0.0
  * http://www.codrops.com
  *
@@ -7,14 +7,58 @@
  * 
  * Copyright 2013, Codrops
  * http://www.codrops.com
- */var ModalEffects=function(){function init(){var overlay=document.querySelector('.md-overlay');overlay.addEventListener('click',function(){$('.md-modal').removeClass('md-show');// classie.remove( document.documentElement, 'md-show' );
-});[].slice.call(document.querySelectorAll('.md-trigger')).forEach(function(el,i){var modal=document.querySelector('#'+el.getAttribute('data-modal'));// var close = modal.querySelector( '.md-close' );
-function removeModal(hasPerspective){classie.remove(modal,'md-show');if(hasPerspective){classie.remove(document.documentElement,'md-perspective')}}function removeModalHandler(){removeModal(classie.has(el,'md-setperspective'))}el.addEventListener('click',function(ev){classie.add(modal,'md-show');// overlay.removeEventListener( 'click', removeModalHandler );
-// overlay.addEventListener( 'click', removeModalHandler );
-if(classie.has(el,'md-setperspective')){setTimeout(function(){classie.add(document.documentElement,'md-perspective')},25)}});// if (close) {
-// 	close.addEventListener( 'click', function( ev ) {
-// 		ev.stopPropagation();
-// 		removeModalHandler();
-// 	});
-// }
-})}init()}();
+ */
+var ModalEffects = (function() {
+
+	function init() {
+
+		var overlay = document.querySelector( '.md-overlay' );
+			overlay.addEventListener( 'click', function() {
+				$('.md-modal').removeClass('md-show');
+				// classie.remove( document.documentElement, 'md-show' );
+			} );
+
+		[].slice.call( document.querySelectorAll( '.md-trigger' ) ).forEach( function( el, i ) {
+
+			var modal = document.querySelector( '#' + el.getAttribute( 'data-modal' ) );
+			// var close = modal.querySelector( '.md-close' );
+
+			function removeModal( hasPerspective ) {
+				classie.remove( modal, 'md-show' );
+
+				if( hasPerspective ) {
+					classie.remove( document.documentElement, 'md-perspective' );
+				}
+			}
+
+			function removeModalHandler() {
+				removeModal( classie.has( el, 'md-setperspective' ) );
+			}
+
+			el.addEventListener( 'click', function( ev ) {
+				classie.add( modal, 'md-show' );
+				// overlay.removeEventListener( 'click', removeModalHandler );
+				// overlay.addEventListener( 'click', removeModalHandler );
+
+				if( classie.has( el, 'md-setperspective' ) ) {
+					setTimeout( function() {
+						classie.add( document.documentElement, 'md-perspective' );
+					}, 25 );
+				}
+			});
+
+
+			// if (close) {
+			// 	close.addEventListener( 'click', function( ev ) {
+			// 		ev.stopPropagation();
+			// 		removeModalHandler();
+			// 	});
+			// }
+
+		} );
+
+	}
+
+	init();
+
+})();
